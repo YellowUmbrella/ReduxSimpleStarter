fc1ed1df1f926fd994c45e2a0329a18a071fcd15
vagnercazarotto <vagnercazarotto@gmail.com>
2018-01-03 18:17:30 -0200

    Class-Based Components

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+class SearchBar extends Component {
+	render(){
+		return <input />;
+	}
+}
 
-const SearchBar = () => {
-	return <input/>;
-};
 
 export default SearchBar;
